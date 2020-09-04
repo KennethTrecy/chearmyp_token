5a94f8da0cdb8d2c47609fdd70188b95ef424acb
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-04 14:57:34 +0800

    Add necessary token and characters for othertongues

--- src/special_characters.rs
+++ src/special_characters.rs
@@ -1,5 +1,7 @@
 pub const COLON: u8 = ':' as u8;
+pub const EQUAL: u8 = '=' as u8;
 pub const NEW_LINE: u8 = '\n' as u8;
 pub const POUND_SIGN: u8 = '#' as u8;
+pub const SPACE: u8 = ' ' as u8;
 pub const TAB: u8 = '\t' as u8;
 pub const VERTICAL_LINE: u8 = '|' as u8;
--- src/token.rs
+++ src/token.rs
@@ -10,6 +10,8 @@ pub enum Token<'a> {
 	Simplex(&'a [u8]),
 	Complex(&'a [u8]),
 	Attacher(&'a [u8], &'a [u8]),
+	LineOthertongue(&'a [u8]),
+	BlockOthertongue(Vec<&'a [u8]>)
 }
 
 /// Contains the extracted token and its last index occupied in the source.
