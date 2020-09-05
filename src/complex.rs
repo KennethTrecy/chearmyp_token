879c4b785c22555ab35afab0d981567f98c9ba3a
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-05 17:14:15 +0800

    Improve test of complex

--- src/complex.rs
+++ src/complex.rs
@@ -47,26 +47,28 @@ pub(crate) fn determine_ending(src: &[u8], offset: usize, limit: usize) -> Delim
 }
 
 #[cfg(test)]
-mod tests {
+mod t {
 	use super::{Token, complex};
+
+	macro_rules! test_complex {
+		($sample:literal, $expected_token:expr, $expected_consumption:literal) => {
+			let (token, consumed_size) = complex($sample, 0, 0);
+			assert_eq!(token, $expected_token);
+			assert_eq!(consumed_size, $expected_consumption);
+		};
+	}
+
+	macro_rules! Complex {
+		($token:literal) => {
+			Token::Complex(&$token[..])
+		};
+	}
 
 	#[test]
 	fn can_lex() {
-		macro_rules! test_complex {
-			(
-				$sample:literal,
-				$expected_token:expr,
-				$expected_consumption:literal
-			) => {
-				let (token, consumed_size) = complex($sample, 0, 0);
-				assert_eq!(token, $expected_token);
-				assert_eq!(consumed_size, $expected_consumption);
-			};
-		}
-
-		test_complex!(b"a", Token::Complex(&b"a"[..]), 0);
-		test_complex!(b"bc	", Token::Complex(&b"bc"[..]), 2);
-		test_complex!(b"d\n", Token::Complex(&b"d"[..]), 1);
-		test_complex!(b"e = f\n", Token::Complex(&b"e"[..]), 1);
+		test_complex!(b"a", Complex!(b"a"), 0);
+		test_complex!(b"bc	", Complex!(b"bc"), 2);
+		test_complex!(b"d\n", Complex!(b"d"), 1);
+		test_complex!(b"e = f\n", Complex!(b"e"), 1);
 	}
 }
