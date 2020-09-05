51afea4b43b73e32cc203f39a28e7c3961f70dfc
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-05 12:52:50 +0800

    Look for othertongues inlined with complex

--- src/complex.rs
+++ src/complex.rs
@@ -1,6 +1,7 @@
 use crate::token::{Token, TokenInfo};
-use crate::special_characters::{NEW_LINE, TAB};
+use crate::special_characters::{NEW_LINE, SPACE, TAB};
 use crate::delimeter::Delimeter;
+use crate::line_othertongue::determine_othertongue_prefix;
 
 pub fn complex(src: &[u8], slice_offset: usize, mut search_offset: usize) -> TokenInfo {
 	let limit = src.len();
@@ -28,6 +29,13 @@ pub fn complex(src: &[u8], slice_offset: usize, mut search_offset: usize) -> Tok
 pub(crate) fn determine_ending(src: &[u8], offset: usize, limit: usize) -> Delimeter {
 	match src[offset] {
 		NEW_LINE | TAB => Delimeter::Pad,
+		SPACE => {
+			if let Delimeter::Pad = determine_othertongue_prefix(src, offset) {
+				Delimeter::Pad
+			} else {
+				Delimeter::Incorrect
+			}
+		},
 		_ => {
 			if offset + 1 == limit {
 				Delimeter::Limit
@@ -59,5 +67,6 @@ mod tests {
 		test_complex!(b"a", Token::Complex(&b"a"[..]), 0);
 		test_complex!(b"bc	", Token::Complex(&b"bc"[..]), 2);
 		test_complex!(b"d\n", Token::Complex(&b"d"[..]), 1);
+		test_complex!(b"e = f\n", Token::Complex(&b"e"[..]), 1);
 	}
 }
