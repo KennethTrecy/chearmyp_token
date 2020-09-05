fcc0b911077b75fd6ccdb7585a6ba60378894202
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-05 13:52:33 +0800

    Refactor block_comment module

--- src/block_comment.rs
+++ src/block_comment.rs
@@ -1,5 +1,5 @@
-use crate::block::block;
 use crate::token::{Token, TokenInfo};
+use crate::block::block;
 use crate::special_characters::POUND_SIGN;
 
 /// Returns the info of recognized block comment and its last seen index in the source.
@@ -38,8 +38,8 @@ use crate::special_characters::POUND_SIGN;
 /// }
 /// assert_eq!(last_index, 0);
 /// ```
-pub fn block_comment(src: &[u8], i: usize, tab_count: usize) -> TokenInfo {
-	let block = block(src, i, tab_count, POUND_SIGN);
+pub fn block_comment(src: &[u8], offset: usize, tab_count: usize) -> TokenInfo {
+	let block = block(src, offset, tab_count, POUND_SIGN);
 	if let (Token::Block(lines), last_seen_index) = block {
 		(Token::BlockComment(lines), last_seen_index)
 	} else {
@@ -49,67 +49,64 @@ pub fn block_comment(src: &[u8], i: usize, tab_count: usize) -> TokenInfo {
 
 #[cfg(test)]
 mod tests {
-	use alloc::vec::Vec;
 	use super::{Token, block_comment};
+
+	macro_rules! test_block_comment {
+		($sample:literal $expected_last_seen_index:literal $expected_token:expr) => {
+			test_block_comment!{
+				sample: $sample,
+				tab_count: 0,
+				consumed_size: $expected_last_seen_index,
+				token: $expected_token
+			}
+		};
+		(
+			sample: $sample:expr,
+			tab_count: $tab_count:literal,
+			consumed_size: $expected_consumed_size:literal,
+			token: $expected_token:expr
+		) => {
+			let (token, block_comment_size) = block_comment(
+				$sample,
+				0,
+				$tab_count
+			);
+			assert_eq!(block_comment_size, $expected_consumed_size,
+				"Consumed size of {:?}", $sample);
+			assert_eq!(token, $expected_token,
+				"Expected token of {:?}", $sample);
+		};
+	}
+
+	macro_rules! BlockComment {
+		($($token:literal)*) => {
+			Token::BlockComment(alloc::vec![$(
+				&$token[..],
+			)*])
+		};
+	}
 
 	#[test]
 	fn can_lex() {
-		macro_rules! test_block_comment {
-			(
-				$sample:literal
-				$expected_consumed_size:literal
-				$expected_token:ident
-			) => {
-				test_block_comment!{
-					sample: $sample,
-					tab_count: 0,
-					consumed_size: $expected_consumed_size,
-					token: $expected_token
-				}
-			};
-			(
-				sample: $sample:expr,
-				tab_count: $tab_count:literal,
-				consumed_size: $expected_consumed_size:literal,
-				token: $expected_token:ident
-			) => {
-				let (token, block_comment_size) = block_comment(
-					$sample,
-					0,
-					$tab_count
-				);
-				assert_eq!(block_comment_size, $expected_consumed_size,
-					"Consumed size of {:?}", $sample);
-				assert_eq!(token, $expected_token,
-					"Expected token of {:?}", $sample);
-			};
-		}
+		test_block_comment!(b"###\n###" 7 BlockComment![]);
 
-		let expected_token = Token::BlockComment(Vec::new());
-		test_block_comment!(b"###\n###" 7 expected_token);
-
-		let expected_token = Token::BlockComment(alloc::vec![&b"hello world!"[..]]);
 		test_block_comment!(
 			sample: b"###\nhello world!\n###",
 			tab_count: 0,
 			consumed_size: 20,
-			token: expected_token);
+			token: BlockComment![b"hello world!"]);
 
-		let expected_token = Token::BlockComment(alloc::vec![&b"\thello world!"[..]]);
 		test_block_comment!(
 			sample: b"###\n\thello world!\n\t###",
 			tab_count: 1,
 			consumed_size: 22,
-			token: expected_token);
+			token: BlockComment![b"\thello world!"]);
 
-		let expected_token = Token::BlockComment(alloc::vec![
-			&b"\thello world!"[..],
-			&b"\t\thi universe"[..]]);
 		test_block_comment!(
 			sample: "###\n\thello world!\n\t\thi universe\n\t\t###".as_bytes(),
 			tab_count: 2,
 			consumed_size: 37,
-			token: expected_token);
+			token: BlockComment![b"\thello world!" b"\t\thi universe"]);
 	}
 
 	#[test]
