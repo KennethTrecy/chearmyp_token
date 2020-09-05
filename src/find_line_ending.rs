cc3d719000643a3ec72ac1bf9a3f2879652be2c4
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-05 13:27:38 +0800

    Remove limit parameter of find_line_ending()

--- src/find_line_ending.rs
+++ src/find_line_ending.rs
@@ -2,31 +2,28 @@ use super::special_characters::NEW_LINE;
 
 /// Returns the index of the first line ending found in the source.
 ///
-/// You can specify where to start looking for the line ending (known as offset) and stop when an
-/// index has been reached (known as limit). If there is no line ending found within the limit, the
-/// limit will returned.
-///
-/// ## Panics
-/// It will panic if the limit is less than the source length.
+/// You can specify where to start looking for the line ending (known as offset). If there is no
+/// line ending found from the offset up to the last index, the source's length will be returned.
 ///
 /// ## Examples
 /// ```
 /// use chearmyp::find_line_ending;
 ///
 /// let a = b"hello world";
-/// assert_eq!(find_line_ending(&a[..], 0, a.len()), 11, "Without line ending");
+/// assert_eq!(find_line_ending(&a[..], 0), 11, "Without line ending");
 ///
 /// let a = b"hello\nworld\n";
-/// assert_eq!(find_line_ending(&a[..], 0, a.len()), 5, "Unskipped line ending");
-/// assert_eq!(find_line_ending(&a[..], 6, a.len()), 11, "Skipped line ending through offset");
+/// assert_eq!(find_line_ending(&a[..], 0), 5, "Unskipped line ending");
+/// assert_eq!(find_line_ending(&a[..], 6), 11, "Skipped line ending through offset");
 /// ```
-pub fn find_line_ending(src: &[u8], offset: usize, limit: usize)-> usize {
-	let mut size = offset;
-
-	while size < limit {
-		if src[size] == NEW_LINE { break; }
-		size += 1;
+pub fn find_line_ending(src: &[u8], mut offset: usize)-> usize {
+	loop {
+		let character = src.get(offset);
+		match character {
+			Some(&NEW_LINE) | None => break,
+			Some(_) => offset += 1
+		}
 	}
 
-	return size;
+	return offset;
 }
