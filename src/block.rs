7449c70177490b95697c00a3af1ec3318c55d8a3
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-05 16:26:16 +0800

    Return empty token for unconsumable source

--- src/block.rs
+++ src/block.rs
@@ -4,37 +4,38 @@ use crate::token::{Token, TokenInfo};
 use crate::special_characters::{NEW_LINE, TAB};
 
 pub fn block(src: &[u8], offset: usize, tab_count: usize, special_character: u8) -> TokenInfo {
-	if !has_3_special_characters(src, offset, special_character) {
-		(Token::Invalid, offset);
-	}
+	if has_3_special_characters(src, offset, special_character) {
+		let mut lines = Vec::new();
+		let mut offset = if let Some(&NEW_LINE) = src.get(offset + 3) { 4 } else { 3 };
 
-	let mut lines = Vec::new();
-	let mut offset = if let Some(&NEW_LINE) = src.get(offset + 3) { 4 } else { 3 };
+		loop {
+			let start = offset;
+			let end = find_line_ending(src, start);
+			if start == end { break; }
+			let line = &src[start..end];
 
-	loop {
-		let start = offset;
-		let end = find_line_ending(src, start);
-		if start == end { break; }
-		let line = &src[start..end];
+			let mut indent_size = tab_count;
+			while indent_size > 0 {
+				indent_size -= 1;
+				if line[indent_size] != TAB { break; }
+			}
 
-		let mut indent_size = tab_count;
-		while indent_size > 0 {
-			indent_size -= 1;
-			if line[indent_size] != TAB { break; }
+			offset = end;
+
+			if indent_size == 0 && has_3_special_characters(line, tab_count, special_character) {
+				if let Some(&NEW_LINE) = src.get(offset) { offset += 1; }
+				break;
+			}
+
+			offset += 1;
+			lines.push(line);
 		}
 
-		offset = end;
-
-		if indent_size == 0 && has_3_special_characters(line, tab_count, special_character) {
-			if let Some(&NEW_LINE) = src.get(offset) { offset += 1; }
-			break;
-		}
-
-		offset += 1;
-		lines.push(line);
+		(Token::Block(lines), offset)
+	} else {
+		let token = if let Some(_) = src.get(offset) { Token::Invalid } else { Token::Empty };
+		(token, offset)
 	}
-
-	(Token::Block(lines), offset)
 }
 
 fn has_3_special_characters(src: &[u8], offset: usize, special_character: u8) -> bool {
