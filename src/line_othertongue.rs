4c8b67a13f54d3a43515afefc9b335f7390071c5
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2020-09-06 19:39:56 +0800

    Document line_othertongue()

--- src/line_othertongue.rs
+++ src/line_othertongue.rs
@@ -3,14 +3,47 @@ use crate::special_characters::{EQUAL, SPACE};
 use crate::delimeter::Delimeter;
 use crate::find_line_ending;
 
-pub fn line_othertongue(src: &[u8], i: usize) -> TokenInfo {
-	match determine_othertongue_prefix(src, i) {
+/// Returns the info of recognized line othertogue and the probably last index that has been checked
+/// from the source.
+///
+/// It needs an array of bytes as the first argument (known as source), and where to start looking
+/// for the line othertongue (inlined or not) as the second argument (known as the offset).
+///
+/// ## Notes
+/// If there is no valid token found, it will return invalid token along with the probably last
+/// index checked.
+///
+/// ## Examples
+/// ```
+/// use chearmyp::line_othertongue;
+/// use chearmyp::token::Token;
+///
+/// let non_terminated = b"= hello world";
+/// let (token, last_index) = line_othertongue(&non_terminated[..], 0);
+/// if let Token::LineOthertongue(token) = token {
+/// 	assert_eq!(token, &b"hello world"[..]);
+/// } else {
+/// 	panic!("The returned token is not line othertongue.");
+/// }
+/// assert_eq!(last_index, 13);
+///
+/// let inlined_yet_terminated = b" = hello world\n";
+/// let (token, last_index) = line_othertongue(&inlined_yet_terminated[..], 0);
+/// if let Token::LineOthertongue(token) = token {
+/// 	assert_eq!(token, &b"hello world"[..]);
+/// } else {
+/// 	panic!("The returned token is not line othertongue.");
+/// }
+/// assert_eq!(last_index, 14);
+/// ```
+pub fn line_othertongue(src: &[u8], offset: usize) -> TokenInfo {
+	match determine_othertongue_prefix(src, offset) {
 		Delimeter::Pad => {
-			let start = i + if src[i] == EQUAL { 2 } else { 3 };
+			let start = offset + if src[offset] == EQUAL { 2 } else { 3 };
 			let end = find_line_ending(src, start);
 			(Token::LineOthertongue(&src[start..end]), end)
 		},
-		_ => (Token::Invalid, i)
+		_ => (Token::Invalid, offset)
 	}
 }
 
