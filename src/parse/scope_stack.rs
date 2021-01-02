7232ed8e232e6ca87271e651407bb4c36546fea5
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-02 11:34:21 +0800

    Integrate Fragment and Relationship to ScopeStack

--- src/parse/scope_stack.rs
+++ src/parse/scope_stack.rs
@@ -3,16 +3,22 @@ mod relationship;
 
 use alloc::vec::Vec;
 use crate::parse::Node;
+use fragment::Fragment;
+use relationship::Relationship;
 
 #[cfg(not(test))]
 pub struct ScopeStack<'a> {
 	level: usize,
+	last_relationship: Relationship,
+	fragments: Vec<Fragment<'a>>,
 	scopes: Vec<Vec<Node<'a>>>
 }
 
 #[cfg(test)]
 pub struct ScopeStack<'a> {
 	pub level: usize,
+	pub last_relationship: Relationship,
+	pub fragments: Vec<Fragment<'a>>,
 	pub scopes: Vec<Vec<Node<'a>>>
 }
 
@@ -20,10 +26,12 @@ impl<'a> ScopeStack<'a> {
 	/// Creates a scope stack that serves as the memory for the main parser.
 	pub fn new() -> Self {
 		let level = 0;
+		let last_relationship = Relationship::Contained;
+		let fragments = Vec::new();
 		let mut scopes = Vec::with_capacity(1);
 
 		scopes.push(Vec::new());
 
-		Self {level, scopes}
+		Self {level, last_relationship, fragments, scopes}
 	}
 }
