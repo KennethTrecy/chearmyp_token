b8c9ffb845dc43427cc58acd74ed6afe2bb5120d
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-02 11:12:12 +0800

    Add ScopeStack structure

--- src/parse.rs
+++ src/parse.rs
@@ -1,13 +1,16 @@
 mod node;
+mod scope_stack;
 
 pub use node::Node;
 
 use alloc::vec::Vec;
 use crate::lex::{Token, TokenQueue};
+use scope_stack::ScopeStack;
 
 pub fn parse<'a, T>(stream: T) -> Vec<Node<'a>>
 where T: 'a + Into<TokenQueue<'a>> {
 	let stream = stream.into();
+	let mut scope_stack = ScopeStack::new();
 
 	for token in stream {
 		match token {
---
+++ src/parse/scope_stack.rs
@@ -0,0 +1,26 @@
+use alloc::vec::Vec;
+use crate::parse::Node;
+
+#[cfg(not(test))]
+pub struct ScopeStack<'a> {
+	level: usize,
+	scopes: Vec<Vec<Node<'a>>>
+}
+
+#[cfg(test)]
+pub struct ScopeStack<'a> {
+	pub level: usize,
+	pub scopes: Vec<Vec<Node<'a>>>
+}
+
+impl<'a> ScopeStack<'a> {
+	/// Creates a scope stack that serves as the memory for the main parser.
+	pub fn new() -> Self {
+		let level = 0;
+		let mut scopes = Vec::with_capacity(1);
+
+		scopes.push(Vec::new());
+
+		Self {level, scopes}
+	}
+}
