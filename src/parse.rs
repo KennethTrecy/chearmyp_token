b67100e647c5d787a0e23fe34b2bf484f5b28f8f
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-02 11:04:38 +0800

    Add initial code for main parser

--- src/parse.rs
+++ src/parse.rs
@@ -1,1 +1,18 @@
 mod node;
+
+pub use node::Node;
+
+use alloc::vec::Vec;
+use crate::lex::{Token, TokenQueue};
+
+pub fn parse<'a, T>(stream: T) -> Vec<Node<'a>>
+where T: 'a + Into<TokenQueue<'a>> {
+	let stream = stream.into();
+
+	for token in stream {
+		match token {
+		}
+	}
+
+	unimplemented!()
+}
