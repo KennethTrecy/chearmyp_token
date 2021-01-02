360184b2a79ac37a14500833f21193875bbf9951
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-02 11:26:03 +0800

    Allow nodes to be attached to fragments

--- src/parse/scope_stack/fragment.rs
+++ src/parse/scope_stack/fragment.rs
@@ -8,3 +8,37 @@ pub enum Fragment<'a> {
 	Simplex(&'a [u8], Vec<Node<'a>>),
 	Complex(&'a [u8], Vec<Node<'a>>)
 }
+
+impl<'a> Fragment<'a> {
+	pub fn attach(&mut self, node: Node<'a>) {
+		match self {
+			Fragment::Simplex(_, attached_nodes) | Fragment::Complex(_, attached_nodes) => {
+				attached_nodes.push(node);
+			}
+		}
+	}
+}
+
+#[cfg(test)]
+mod t {
+	use alloc::vec::Vec;
+	use super::{Node, Fragment};
+
+	#[test]
+	fn can_attach() {
+		let concept_name = b"Attaching attachers to simplex fragments";
+		let label = b"ab";
+		let content = b"cd";
+		let mut fragment = Fragment::Simplex(&concept_name[..], Vec::new());
+		let attacher = Node::Attacher(&label[..], &content[..]);
+		let expected_fragment = Fragment::Simplex(&concept_name[..], {
+			let mut attachers = Vec::new();
+			attachers.push(Node::Attacher(&label[..], &content[..]));
+			attachers
+		});
+
+		fragment.attach(attacher);
+
+		assert_eq!(fragment, expected_fragment);
+	}
+}
