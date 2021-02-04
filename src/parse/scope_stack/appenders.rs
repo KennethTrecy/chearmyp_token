59e5d5d3bf89dd3175189b570b0e24133f5e53e5
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-02-04 10:39:24 +0800

    Remove module related to parser

--- src/parse/scope_stack/appenders.rs
+++
@@ -1,9 +0,0 @@
-use super::ScopeStack;
-
-mod append_complex;
-mod append_simplex;
-mod append_attacher;
-mod append_line_comment;
-mod append_block_comment;
-mod append_line_othertongue;
-mod append_block_othertongue;
--- src/parse/scope_stack/relationship.rs
+++
@@ -1,6 +0,0 @@
-/// Indicates the relationship of latest node created to the latest fragment.
-#[cfg_attr(test, derive(Debug, PartialEq))]
-pub enum Relationship {
-	Attached,
-	Contained
-}
