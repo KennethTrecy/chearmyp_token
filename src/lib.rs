da81be2eb67cb60f6161280d45f6d5d5e3acee31
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-08 16:31:49 +0800

    Prepare abstracts to implement

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
 extern crate alloc;
 
+mod abstracts {
+	pub use abstract_chearmyp_token::AbstractToken;
+}
+
 /// Contains the data structures and type aliases used and/or returned by some lexers. They can be
 /// used by both lexers and parsers.
 mod token;
