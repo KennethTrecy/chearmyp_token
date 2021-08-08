baf864a94d43ec7b2fe455625e112ceaffc5cbf7
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-08 16:28:48 +0800

    Generalize token

--- src/token.rs
+++ src/token.rs
@@ -1,14 +1,12 @@
-use alloc::vec::Vec;
-
 /// Contains the valid tokens used for lexing and parsing.
 #[derive(Debug, PartialEq)]
-pub enum Token<'a> {
+pub enum Token<T, U> {
 	ScopeLevel(usize),
-	LineComment(&'a [u8]),
-	BlockComment(Vec<&'a [u8]>),
-	Simplex(&'a [u8]),
-	Complex(&'a [u8]),
-	Attacher(&'a [u8], &'a [u8]),
-	LineOthertongue(&'a [u8]),
-	BlockOthertongue(Vec<&'a [u8]>)
+	LineComment(T),
+	BlockComment(U),
+	Simplex(T),
+	Complex(T),
+	Attacher(T, T),
+	LineOthertongue(T),
+	BlockOthertongue(U)
 }
